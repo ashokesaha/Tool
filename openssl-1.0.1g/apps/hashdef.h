f81f657a8526563e2e7800318edbe34e57f47c7a
ashoke saha <ashoke@rocketmail.com>
2016-02-13 17:16:32 +0530

    adding hashdef.h

---
+++ openssl-1.0.1g/apps/hashdef.h
@@ -0,0 +1,70 @@
+#define	 JLogin							1
+#define	 JAddHTTPVserver				101
+#define	 JDelHTTPVserver				102
+#define	 JAddSSLVserver					103
+#define	 JDelSSLVserver					104
+
+#define	 JAddHTTPService				105
+#define	 JDelHTTPService				106
+#define	 JAddSSLService					107
+#define	 JDelSSLService					108
+
+#define	 JAddCertKey					109
+#define	 JDelCertKey					110
+
+#define	 JBindUnbindrCertkey			111
+
+#define	 JBindSSLVserverCertkey			111
+#define	 JUnbindSSLVserverCertkey		112
+#define	 JBindSSLServiceCertkey			113
+#define	 JUnbindSSLServiceCertkey		114
+
+#define	 JBindSSLVserverCACertkey		115
+#define	 JUnbindSSLVserverCACertkey		116
+#define	 JBindSSLServiceCACertkey		117
+#define	 JUnbindSSLServiceCACertkey		118
+
+#define	 JBindSSLVserverSNICertkey		119
+#define	 JUnbindSSLVserverSNICertkey	120
+#define	 JBindSSLServiceSNICertkey		121
+#define	 JUnbindSSLServiceSNICertkey	122
+
+#define	 JBindUnbindCipher				123
+#define	 JUnbindAllCipher				124
+
+#define	 JBindSSLVserverCipher			123
+#define	 JUnbindSSLVserverCipher		124
+#define	 JBindSSLServiceCipher			124
+#define	 JUnbindSSLServiceCipher		125
+
+#define	 JEnableSSLVserverCauth			126
+#define	 JDisableSSLVserverCauth		127
+#define	 JEnableSSLServiceCauth			128
+#define	 JDisableSSLServiceCauth		129
+
+#define	 JEnableSSLVserverSessReuse		130
+#define	 JDisableSSLVserverSessReuse	131
+#define	 JEnableSSLServiceSessReuse		132
+#define	 JDisableSSLServiceSessReuse	133
+
+#define	 JEnableSSLVserverDH			134
+#define	 JDisableSSLVserverDH			135
+#define	 JEnableSSLServiceDH			136
+#define	 JDisableSSLServiceDH			137
+
+#define	 JSetSSLVserverSessionTimeout	138
+#define	 JSetSSLServiceSessionTimeout	139
+
+#define	 JEnableSSLVserverVersion		140
+#define	 JDisableSSLVserverVersion		141
+#define	 JEnableSSLServiceVersion		142
+#define	 JDisableSSLServiceVersion		143
+
+#define	 JAddCRL						144
+#define	 JDelCRL						145
+
+#define	 JAddOCSPResponder				146
+#define	 JDelOCSPResponder				147
+
+#define	 JAddServer						148
+#define	 JDelServer						149
